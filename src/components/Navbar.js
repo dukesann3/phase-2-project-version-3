6929bbe939701b7a69bf6cfd83e25c923ac104ce
Duke <dukemorise33@gmail.com>
2023-11-22 23:18:04 -0800

    added navigation bar elements: home/about/settings/posts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
+import { NavLink } from "react-router-dom";
 
 function Navbar(){
     return(
         <>
+            <NavLink to='/'>HOME</NavLink>
+            <NavLink to='/About'>ABOUT</NavLink>
+            <NavLink to='/Settings'>SETTINGS</NavLink>
+            <NavLink to='/Posts'>POSTS</NavLink>
         </>
     )
 }
