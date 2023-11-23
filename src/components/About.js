d5b4e0f3d0c549191c0fc9ae4dd3765e40f0d82b
Duke <dukemorise33@gmail.com>
2023-11-22 23:25:09 -0800

    added custom useFetchPosts hook onto Postlist and mapped <Post />

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@
 function About(){
     return(
         <>
+        
         </>
     )
 }
--- src/components/Postlist.js
+++ src/components/Postlist.js
@@ -1,7 +1,14 @@
+import useFetchPosts from "../custom_hooks/useFetch";
+import Post from "./Post";
 
 function Postlist(){
+
+    const apiUrl = 'http://localhost:8000/posts';
+    const [posts, setPosts] = useFetchPosts(apiUrl);
+
     return(
         <>
+            {posts.map(post => <Post post={post} />)}
         </>
     )
 }
