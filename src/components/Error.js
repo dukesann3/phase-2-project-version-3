913de752e68817a30defd5fb0adc67e98c485a35
Duke <dukemorise33@gmail.com>
2023-12-07 22:53:56 -0800

    added navigate for error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Error(){
+
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            navigate(-1);
+        }, 3000);
+    },[]);
+
     return(
         <>
             <h1>ERROR 404, PAGE NOT FOUND</h1>
